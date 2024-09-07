5a18ba70308ade8e1132508b751720e502d6c906
Maral <erdenemaral@grinnell.edu>
2024-09-07 14:13:20 -0500

    3rd try

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, Maralmaa!");
+    pen.println ("Hello, Maral ;) !");
     pen.flush();
     pen.close();
   } // main(String[])
