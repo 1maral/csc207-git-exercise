316366a5eca6be0ecc3fdede1b3f4314bd9469e8
maral13 <158646241+maral13@users.noreply.github.com>
2024-09-07 14:04:05 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Maral, Nice to meet you!");
+        System.out.println("Hello, Maral, Thanks too!");
     }
 }
